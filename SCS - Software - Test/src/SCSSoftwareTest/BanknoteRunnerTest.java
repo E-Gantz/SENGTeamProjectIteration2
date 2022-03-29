8371e2e8207696f6868e1c1daeebf31f681b2495
Evan Wong <evan.wong1@ucalgary.ca>
2022-03-28 19:11:18 -0600

    tests

--- SCS - Software - Test/src/SCSSoftwareTest/BanknoteRunnerTest.java
+++ SCS - Software - Test/src/SCSSoftwareTest/BanknoteRunnerTest.java
@@ -7,6 +7,7 @@ import java.util.Currency;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.lsmr.selfcheckout.Banknote;
 import org.lsmr.selfcheckout.Barcode;
 import org.lsmr.selfcheckout.BarcodedItem;
 import org.lsmr.selfcheckout.Numeral;
@@ -15,6 +16,10 @@ import org.lsmr.selfcheckout.devices.BanknoteSlot;
 import org.lsmr.selfcheckout.devices.BanknoteStorageUnit;
 import org.lsmr.selfcheckout.devices.BanknoteValidator;
 import org.lsmr.selfcheckout.devices.BarcodeScanner;
+import org.lsmr.selfcheckout.devices.BidirectionalChannel;
+import org.lsmr.selfcheckout.devices.DisabledException;
+import org.lsmr.selfcheckout.devices.OverloadException;
+import org.lsmr.selfcheckout.devices.UnidirectionalChannel;
 import org.lsmr.selfcheckout.products.BarcodedProduct;
 
 import SCSSoftware.BanknoteRunner;
@@ -39,6 +44,7 @@ public class BanknoteRunnerTest {
 	private BanknoteValidator bValidator;
 	private int[] banknoteDenom = {5, 10, 20, 50, 100};
 	private Currency currency = Currency.getInstance("CAD");
+	private BidirectionalChannel<Banknote> validatorSource;
 	
 	@Before
 	public void setup() {
@@ -50,17 +56,37 @@ public class BanknoteRunnerTest {
 		bStorage = new BanknoteStorageUnit(1000);
 		bValidator = new BanknoteValidator(currency, banknoteDenom);
 		
+		interconnect(bSlot, bValidator);
+		interconnect(bValidator, bStorage);
+		
 		bSlot.endConfigurationPhase();
 		bStorage.endConfigurationPhase();
 		bValidator.endConfigurationPhase();
 		
 		banknoteRunner = new BanknoteRunner(checkout.getTotalPrice(), bSlot, bStorage, bValidator);
 	}
+	
+	private void interconnect(BanknoteSlot slot, BanknoteValidator validator) {
+		validatorSource = new BidirectionalChannel<Banknote>(slot, validator);
+		slot.connect(validatorSource);
+	}
+
+	private void interconnect(BanknoteValidator validator, BanknoteStorageUnit storage) {
+		UnidirectionalChannel<Banknote> bc = new UnidirectionalChannel<Banknote>(storage);
+		validator.connect(validatorSource, bc);
+	}
 
 	@Test
 	public void testGetCheckoutTotal() {
 		scanner.scan(item1);
 		assertEquals(banknoteRunner.getCheckoutTotal(), checkout.getTotalPrice());
+	}
+	
+	@Test
+	public void testGetPaidTotal() throws DisabledException, OverloadException {
+		Banknote note = new Banknote(Currency.getInstance("CAD"), 5);
+		bSlot.accept(note);
+		assert(banknoteRunner.getPaidTotal().equals(BigDecimal.valueOf(note.getValue())));
 	}
 
 }
