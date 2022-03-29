c2804947d4b8f33db359fbf7717ef6f165f6fd79
PKDT-93 <75346912+PKDT-93@users.noreply.github.com>
2022-03-28 19:05:52 -0600

    New methods
    Co-Authored-By: Collinnh <101361206+Collinnh@users.noreply.github.com>
    Co-Authored-By: Evan Wong <43582348+evannwong@users.noreply.github.com>

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/PaysWithCash.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/PaysWithCash.java
@@ -2,24 +2,49 @@ package SCSSoftware;
 
 import java.math.BigDecimal;
 
+import org.lsmr.selfcheckout.devices.BanknoteDispenser;
+import org.lsmr.selfcheckout.devices.CoinDispenser;
+
 public class PaysWithCash {
 	
 	private PaysWithCoin payswithcoin;
 	private BanknoteRunner banknoterunner;
+	private BanknoteDispenser banknotedispenser;
+	private CoinDispenser coindispenser;
+	private BigDecimal chargedTotal;
+	private BigDecimal totalValue;
 	
-	public PaysWithCash(PaysWithCoin payswithCoin, BanknoteRunner banknoteRunner)
+	
+	
+	public PaysWithCash(PaysWithCoin payswithCoin, BanknoteRunner banknoteRunner, BanknoteDispenser banknoteDispenser, 
+			CoinDispenser coinDispenser)
 	{
 		this.payswithcoin = payswithCoin;
 		this.banknoterunner = banknoteRunner;
+		this.coindispenser = coinDispenser;
+		this.banknotedispenser = banknoteDispenser;
 	}
 	
-	public BigDecimal sumCoinBanknote()
+	public void sumCoinBanknote()
 	{
 		BigDecimal totalCoins = new BigDecimal((payswithcoin.getTotalCoins()).floatValue());
 		BigDecimal totalBanknotes = new BigDecimal((banknoterunner.sumBanknotes().floatValue()));
-		BigDecimal totalValue =  new BigDecimal(0).add(totalCoins);
+		totalValue =  new BigDecimal(0).add(totalCoins);
 		totalValue.add(totalBanknotes);
-		return totalValue;
+	}
+	
+	public void getChange(BanknoteRunner banknoterunner)
+	{
+		chargedTotal = new BigDecimal((banknoterunner.getCheckoutTotal().floatValue()));
+		totalValue.subtract(chargedTotal);
+	}
+	
+	public void emitChange()
+	{
+		if(totalValue.compareTo(BigDecimal.ZERO) >= 0)
+		{
+			
+		}
 	}
 	
 }
